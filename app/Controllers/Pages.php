af0627fd5b7eacb2ab6d793811f1b27d77047602
reynaldioktino <reynaldiolp@gmail.com>
2021-07-06 10:49:54 +0700

    CI4 Views

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        $data = [
+            'title' =>  'Home',
+            'tes'   =>  ['satu', 'dua', 'tiga']
+        ];
+        echo view('layout/header', $data);
+        echo view('pages/home');
+        echo view('layout/footer');
+    }
+
+    public function about()
+    {
+        $data = [
+            'title' =>  'About Us'
+        ];
+        echo view('layout/header', $data);
+        echo view('pages/about');
+        echo view('layout/footer');
+    }
+}
---
+++ app/Views/pages/home.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Hello, World!</h1>
+            <?php
+            foreach ($tes as $value) {
+                echo $value . '<br>';
+            }
+            ?>
+        </div>
+    </div>
+</div>
